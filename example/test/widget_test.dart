2634f3511b5f8380ce702def6581fd65f7228b5e
qminhminh <hqminh050503@gmail.com>
2025-11-13 17:32:45 +0700

    Update widget_test.dart to test dynamic form functionality and validation warnings

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,26 +5,24 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:example/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Displays dynamic form and shows validation warning',
+      (tester) async {
+    await tester.pumpWidget(const FromDataExampleApp());
+    await tester.pumpAndSettle();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Service Registration'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(find.text('Submit form'));
+    await tester.pump(const Duration(milliseconds: 100));
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(
+      find.text('Please double-check the required fields.'),
+      findsOneWidget,
+    );
   });
 }
